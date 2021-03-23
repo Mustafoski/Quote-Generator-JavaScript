04e9b645038971171b05ca7effa768af3f16db3f
Emir <emirmustafoski92@gmail.com>
2021-03-23 20:13:48 +0100

    changes

--- script.js
+++ script.js
@@ -23,12 +23,12 @@ function complete() {
 // Get Quote from APi
 async function getQuote() {
   loading();
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+//  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
   try {
-    const response = await fetch(proxyUrl + apiUrl);
+    const response = await fetch( apiUrl);
     const data = await response.json();
 
     // if author is blank or unknown
