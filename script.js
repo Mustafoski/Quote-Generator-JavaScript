2f3ef3b346a76afdf98d102ee7b0a05e3394b688
Emir Mustafoski <emirmustafoski92@gmail.com>
2021-08-03 08:07:30 +0200

    dsada
    dsad

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function getQuote() {
   loading();
 //  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    'http://api.forismatic.com/api/1.0/';
 
   try {
     const response = await fetch( apiUrl);
